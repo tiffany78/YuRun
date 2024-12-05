5cd87a5df49ec2b40d19a9b0159e5195130e47cd
TiffanyTasya <6182201017@student.unpar.ac.id>
2024-12-05 09:34:07 +0700

    Create skeleton of Project YuRun

---
+++ src/main/java/com/example/YuRun/YuRunApplication.java
@@ -0,0 +1,13 @@
+package com.example.YuRun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuRunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YuRun/YuRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YuRun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
