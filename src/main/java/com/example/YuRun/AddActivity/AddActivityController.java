158a5acc303ef71ae31a576b72dbe67b17bf43f8
TiffanyTasya <6182201017@student.unpar.ac.id>
2024-12-09 15:29:26 +0700

    Edit mapping

--- src/main/java/com/example/YuRun/AddActivity/AddActivityController.java
+++ src/main/java/com/example/YuRun/AddActivity/AddActivityController.java
@@ -3,12 +3,14 @@ package com.example.YuRun.AddActivity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class AddActivityController {
 
     @GetMapping("/addActivity")
-    public String showAddActivityPage(){
-        return "/AddActivity/AddActivityPage";
+    public String index(){
+        return "/AddActivity/index2";
     }
 }
