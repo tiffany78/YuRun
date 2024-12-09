0bb99bb1c13cb4830590dd4e74dd83b61cfb108b
TiffanyTasya <6182201017@student.unpar.ac.id>
2024-12-09 15:20:22 +0700

    Edit UserInformation Admin html

--- src/main/java/com/example/YuRun/Admin/UserInformation/UserInformationController.java
+++ src/main/java/com/example/YuRun/Admin/UserInformation/UserInformationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin/userInformation")
+@RequestMapping("/admin")
 public class UserInformationController {
     @Autowired
     private final UserInformationRepository userInformationRepository;
@@ -19,13 +19,22 @@ public class UserInformationController {
         this.userInformationRepository = userInformationRepository;
     }
 
-    @GetMapping
-    public String getSetoranMembers(@RequestParam(value = "filter", required = false) String filter,
-                                     Model model) {
-        model.addAttribute("filter", filter);
-        List<User> setoranList = userInformationRepository.findAll(filter);
+    // @GetMapping
+    // public String getSetoranMembers(@RequestParam(value = "filter", required = false) String filter, Model model) {
+    //     model.addAttribute("filter", filter);
+    //     List<User> setoranList = userInformationRepository.findAll(filter);
 
-        model.addAttribute("setoranList", setoranList);
-        return "/Admin/UserInformationAdmin/index"; 
+    //     model.addAttribute("setoranList", setoranList);
+    //     return "/Admin/UserInformationAdmin/index"; 
+    // }
+
+    @GetMapping("/info1")
+    public String getSetoranMembers() {
+        return "/Admin/UserInformation/index2"; 
+    }
+
+    @GetMapping("/info2")
+    public String index() {
+        return "/Admin/UserInformation/index"; 
     }
 }
