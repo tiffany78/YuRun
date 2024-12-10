90b86b74dc50db9173b4e1652351619c98cb1515
Rayhan Adji Santoso <rayhanadjis@gmail.com>
2024-12-10 20:37:22 +0700

    menyelesaikan register

--- src/main/java/com/example/YuRun/Register/RegisterUser.java
+++ src/main/java/com/example/YuRun/Register/RegisterUser.java
@@ -1,14 +1,15 @@
-package com.example.YuRun.Login;
+package com.example.YuRun.Register;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class LoginUser {
+public class RegisterUser {
     private String name;
     private String email;
     private String password;
+    private String confirmPassword;
     private boolean isadmin;
     private boolean status;
 }
