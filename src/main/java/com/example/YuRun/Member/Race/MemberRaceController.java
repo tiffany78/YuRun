c4b450afa272c3b4ae10a04f6b13482b962cab4a
Rayhan Adji Santoso <rayhanadjis@gmail.com>
2024-12-22 21:06:49 +0700

    html race-user

---
+++ src/main/java/com/example/YuRun/Member/Race/MemberRaceController.java
@@ -0,0 +1,15 @@
+package com.example.YuRun.Member.Race;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberRaceController {
+    @GetMapping("/race")
+    public String race() {
+        return "/Member/Race/index";
+    }
+}
+    
