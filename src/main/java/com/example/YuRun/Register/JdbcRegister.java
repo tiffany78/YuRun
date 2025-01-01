7c24ca0593e3825dbb7598d23983e8939bf793e8
Fernando Indrawan <frostycyber012@gmail.com>
2025-01-02 00:04:27 +0700

    test

--- src/main/java/com/example/YuRun/Register/JdbcRegister.java
+++ src/main/java/com/example/YuRun/Register/JdbcRegister.java
@@ -3,7 +3,6 @@ package com.example.YuRun.Register;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
